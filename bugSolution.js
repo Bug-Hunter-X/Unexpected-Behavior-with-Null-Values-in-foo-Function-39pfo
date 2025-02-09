472b6e4b04dc026a58f231da8862eb6626099107
BugHunter-X <askzenai@gmail.com>
2025-02-09 01:28:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,5 @@
+function foo(a, b) {
+  a = a === null ? 0 : a; // Assign default value if a is null
+  b = b === null ? 0 : b; // Assign default value if b is null
+  // ... rest of the function logic
+}
